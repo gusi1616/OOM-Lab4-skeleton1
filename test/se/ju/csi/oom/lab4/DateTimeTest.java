71317dde4bceb5cc9d47bab1e0b38cbd47a6a29b
gusi1616 <gusi1616@users.noreply.github.com>
2018-10-21 15:27:54 +0200

    dateTimeTest update
    added two new dateTimeTests

---
+++ test/se/ju/csi/oom/lab4/DateTimeTest.java
@@ -0,0 +1,30 @@
+package se.ju.csi.oom.lab4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DateTimeTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testToString() {
+		DateTime _dateTime = new DateTime(2000, 1, 1, 10, 0, 0);
+		assertEquals(new String("2000-01-01 10:00:00"), _dateTime.toString());
+		System.out.println(_dateTime.toString());
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDateTimeString() {
+		DateTime _dateTime = new DateTime(2012, 12, 12, 12, 12, 12);
+		assertEquals(new String("2012-12-12 12:12:12"), _dateTime.toString());
+		System.out.println(_dateTime.toString());
+//		fail("Not yet implemented");
+	}
+
+}
